be015028eb796ce09e348cdea197d5fc6e504f96
BhavaniPonnuri <bhavaniponnuri08@gmail.com>
2026-03-20 18:16:04 +0530

    Final commit- remove trackeback

--- app.py
+++ app.py
@@ -23,28 +23,25 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        try: 
-            data=CustomData(
-                gender = request.form.get('gender'),
-                race_ethnicity=request.form.get('ethnicity'),
-                parental_level_of_education=request.form.get('parental_level_of_education'),
-                lunch=request.form.get('lunch'),
-                test_preparation_course=request.form.get('test_preparation_course'),
-                reading_score=float(request.form.get('writing_score')),
-                writing_score=float(request.form.get('reading_score'))
+        data=CustomData(
+            gender = request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
         )
         
-            pred_df = data.get_data_as_dataframe()
-            print(pred_df)
-            print("Before Prediction")
+        pred_df = data.get_data_as_dataframe()
+        print(pred_df)
+        print("Before Prediction")
         
-            predict_pipeline =  PredictPipeline()
-            print("Mid Prediction")
-            results = predict_pipeline.predict(pred_df)
-            print("After prediction")
-            return render_template("home.html", results = results[0])
-        except Exception as e:
-            return f"<pre>{traceback.format_exc()}</pre>"
+        predict_pipeline =  PredictPipeline()
+        print("Mid Prediction")
+        results = predict_pipeline.predict(pred_df)
+        print("After prediction")
+        return render_template("home.html", results = results[0])
     
 if __name__=="__main__":
     port = int(os.environ.get('PORT', 5000))
